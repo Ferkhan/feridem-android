034035fac1c93cc8036ea7c05939269197fceb49
Ferkhan <133190285+Ferkhan@users.noreply.github.com>
2023-07-23 02:26:47 -0500

    cambiar activity iniciar sesion y registro, agregar funcionalidad de cambio de pantalla

--- app/src/main/java/com/feridem/android/MainActivity.java
+++ app/src/main/java/com/feridem/android/MainActivity.java
@@ -2,13 +2,25 @@ package com.feridem.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button botonRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botonRegistrarse = findViewById(R.id.botonRegistrarse);
+        botonRegistrarse.setOnClickListener(this::irRegistro);
+    }
+
+    private void irRegistro(View view) {
+        Intent siguiente = new Intent(this, RegistroActivity.class);
+        startActivity(siguiente);
     }
 }
--- app/src/main/java/com/feridem/android/RegistroActivity.java
+++ app/src/main/java/com/feridem/android/RegistroActivity.java
@@ -2,13 +2,25 @@ package com.feridem.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistroActivity extends AppCompatActivity {
+    private Button botonIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        botonIniciarSesion = findViewById(R.id.botonIniciarSesion);
+        botonIniciarSesion.setOnClickListener(this::irIniciarSesion);
+    }
+
+    private void irIniciarSesion(View vista) {
+//        Intent siguiente = new Intent(this, MainActivity.class);
+        onBackPressed();
     }
 }
