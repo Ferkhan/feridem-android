63e3bef4a39cafca96c7c41bd6e62004e8e48ddd
Ferkhan <133190285+Ferkhan@users.noreply.github.com>
2023-09-10 23:36:36 -0500

    borrar basura en PerfilFragment

--- app/src/main/java/com/feridem/android/user_interface/fragment/PerfilFragment.java
+++ app/src/main/java/com/feridem/android/user_interface/fragment/PerfilFragment.java
@@ -25,13 +25,10 @@ import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PerfilFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class PerfilFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private Button
@@ -42,33 +39,12 @@ public class PerfilFragment extends Fragment {
             nomUsuario,
             correoUs,
             celularUs;
-    private String
-            mParam1,
-            mParam2;
     private UsuarioBL usuarioBL;
     private RegistroSesionBL registroSesionBL;
     private Usuario usuario;
 
     public PerfilFragment() {
         // Constructor vacío
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PerfilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PerfilFragment newInstance(String param1, String param2) {
-        PerfilFragment fragment = new PerfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     /**
@@ -79,11 +55,6 @@ public class PerfilFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     /**
